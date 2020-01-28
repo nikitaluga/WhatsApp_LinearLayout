24236c9ee99781036f5ceeef32f3b83f19ac1b27
lugovoyNikita <zenit27069621@mail.ru>
2020-01-28 14:21:19 +0300

    Value of the spinner is set in TextView

--- app/src/main/java/com/example/homework_whatsapp_linearlayout/MainActivity.java
+++ app/src/main/java/com/example/homework_whatsapp_linearlayout/MainActivity.java
@@ -6,24 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewMassage;
+    EditText editTextMassage;
+
+    Spinner spinnerName;
+    Spinner spinnerGroup;
+
+    TextView textViewName;
+    TextView textViewGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onClickMyButton(View view) {
-        TextView textView = findViewById(R.id.myTextViewMassage);
-        EditText editText = findViewById(R.id.myEditText);
+        textViewMassage = findViewById(R.id.myTextViewMassage);
+        editTextMassage = findViewById(R.id.myEditText);
+        String textFromEditText = editTextMassage.getText().toString();
+        textViewMassage.setText(textFromEditText);
 
-        String textFromEditText = editText.getText().toString();
+        spinnerName = findViewById(R.id.spinnerName);
+        String textFromSpinnerName = spinnerName.getSelectedItem().toString();
+        textViewName = findViewById(R.id.textViewName);
+        textViewName.setText(textFromSpinnerName);
 
-        textView.setText(textFromEditText);
+        spinnerGroup = findViewById(R.id.spinnerGroup);
+        String textFromSpinnerGroup = spinnerGroup.getSelectedItem().toString();
+        textViewGroup = findViewById(R.id.textViewGroup);
+        textViewGroup.setText(textFromSpinnerGroup);
     }
-
-
 }
